a509fb26e490624f840511c07f22b8a4ddffcdf2
Patrick Pio <your-email@example.com>
2026-04-28 11:09:02 +0530

    my updates

---
+++ src/app/car-view/car-view.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-view',
+  imports: [],
+  templateUrl: './car-view.html',
+  styleUrl: './car-view.css',
+})
+export class CarViewComponent {}
