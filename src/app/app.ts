de99277dcc7971bf79dd47ba55a3a6f472ed0677
alvinvtharian-prog <alvinvtharian2@gmail.com>
2026-05-11 12:13:40 +0530

    Added reusable navbar component and fixed shared layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class AppComponent {}
