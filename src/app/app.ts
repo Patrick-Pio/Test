293ae68a159fcae899a10a83b88897572ba6d786
alvinvtharian-prog <alvinvtharian2@gmail.com>
2026-05-11 12:36:19 +0530

    Updated reusable navbar and hidden navbar on login pages

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, RouterModule, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs';
 import { NavbarComponent } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, RouterModule, CommonModule, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class AppComponent {}
+export class AppComponent {
+  showNavbar = true;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.showNavbar =
+          event.urlAfterRedirects !== '/login' &&
+          event.urlAfterRedirects !== '/register' &&
+          event.urlAfterRedirects !== '/';
+      });
+  }
+}
